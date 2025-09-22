35310e071482a042948395fb4c9c150b34688d6c
sonofbigdreams-ctrl <sonofbigdreams@gmail.com>
2025-09-22 11:35:27 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: BADGUY
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "BADGUY
+  botOwner:BADGUY
+  ownerNumber: '233500692498 //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
